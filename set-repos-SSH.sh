81050a86001bd707cfdee7c2246d0f2c1b448274
NikoboiNFTB <promanfin05@gmail.com>
2025-10-27 16:45:00 +0200

    Auto-update: staged changes

--- set-repos-SSH.sh
+++ set-repos-SSH.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 BASE=~/Documents/GitHub
+GITHUB_USER="NikoboiNFTB"
 
 for dir in "$BASE"/*; do
     if [ -d "$dir/.git" ]; then
         cd "$dir" || continue
-        HTTPS_URL=$(git remote get-url origin)
-        # Only switch if it’s HTTPS
-        if [[ $HTTPS_URL == https://github.com/* ]]; then
-            # Convert HTTPS to SSH
-            REPO_NAME=$(basename "$dir")
-            SSH_URL="git@github.com:$(basename $(dirname "$dir"))/$REPO_NAME.git"
-            git remote set-url origin "$SSH_URL"
-            echo "Switched $dir to SSH"
-        fi
+        REPO_NAME=$(basename "$dir")
+        SSH_URL="git@github.com:$GITHUB_USER/$REPO_NAME.git"
+        git remote set-url origin "$SSH_URL"
+        echo "Set SSH URL for $dir to $SSH_URL"
     fi
 done
