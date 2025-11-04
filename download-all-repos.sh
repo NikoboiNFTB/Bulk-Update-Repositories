dbf9ca421aab1b5a27689798411882cf2405880a
NikoboiNFTB <promanfin05@gmail.com>
2025-11-04 15:26:17 +0200

    Auto-commit: 2025-11-04 15:26:17

--- download-all-repos.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Updating all repositories..."
-find . -maxdepth 1 -mindepth 1 -type d -exec test -d "{}/.git" \; -print | xargs -P0 -I{} sh -c 'git -C "{}" pull >/dev/null 2>&1'
-echo "Done!"
