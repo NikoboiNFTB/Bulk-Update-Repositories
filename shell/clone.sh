2614b5e5318d07a735c3476aa2dacc53ea9249d8
NikoboiNFTB <github@nikoboi.dev>
2025-12-09 16:08:24 +0200

    Automated push

--- shell/clone.sh
+++ shell/clone.sh
@@ -40,7 +40,7 @@ echo "All done!"
 #           Right click -> Copy
 #
 #  2. Run: nano clone.sh
-#     Edit author="" and repos=() at the top according to instructions next to each
+#     Edit author and repos at the top according to instructions next to each
 #     To save using nano, hit "Ctrl + O", "Enter", "Ctrl + X"
 #
 #  3. Run: ./clone.sh && rm clone.sh
