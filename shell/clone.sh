37b4e5b6189631df0f7ea60128295ad99133559f
NikoboiNFTB <github@nikoboi.dev>
2025-12-09 16:09:24 +0200

    Automated push

--- shell/clone.sh
+++ shell/clone.sh
@@ -43,7 +43,7 @@ echo "All done!"
 #     Edit author and repos at the top according to instructions next to each
 #     To save using nano, hit "Ctrl + O", "Enter", "Ctrl + X"
 #
-#  3. Run: ./clone.sh && rm clone.sh
+#  3. Run: ./clone.sh
 #     Note: Cloning many repositories might take a while.
 #
 #  <======== GUIDE END ========>
